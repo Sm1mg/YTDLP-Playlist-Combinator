d6823be077463ee22ddfb00e3fb3cf76fb3d7e9d
Sm1mg <43554685+Sm1mg@users.noreply.github.com>
2023-05-20 00:54:42 -0400

    QoL tweaks

--- YTDLP_Playlist_Combinator.py
+++ YTDLP_Playlist_Combinator.py
@@ -1,6 +1,7 @@
 import yt_dlp
 import os
 import random
+import sys
 def monitor(d):
     filenames.append(d.get('info_dict').get('_filename'))
 def clear_working_path():
@@ -8,7 +9,10 @@ def clear_working_path():
         os.remove(f'{working_path}\\{file}')
     os.rmdir(working_path)
 
-url = "https://music.youtube.com/playlist?list=OLAK5uy_mO-B6-lxdrHnNLB3vPPLRQVdzDbAZgHxc"
+if len(sys.argv) == 1:
+    url = "https://music.youtube.com/playlist?list=OLAK5uy_nmDUsWOMoEcz0SsVqUwir0oxu-k1oUyXE"
+else:
+    url = sys.argv[1]
 dlp_options = {
         'format': 'bestvideo*+bestaudio/best',
         'nocheckcertificate': True,
@@ -34,14 +38,13 @@ print("Working path created")
 os.chdir(working_path)
 
 # Download playlist
-yt_dlp.YoutubeDL(dlp_options).download(url)
+playlist = yt_dlp.YoutubeDL(dlp_options).extract_info(url)
 
 # Briefly convert to dict to remove duplicates because of some weird yt_dlp shit
 filenames = list(dict.fromkeys(filenames))
 
 # Rename all the files because ffmpeg pissy otherwise
 for i, file in enumerate(filenames):
-    print(filenames)
     os.rename(file, f'{i}.webm')
     filenames[i] = f'{i}.webm'
 
@@ -52,6 +55,8 @@ with open("super_cool_list.txt", 'w') as list:
     for file in filenames:
         list.write(f"file '{file}'\n")
 
-os.system("ffmpeg -safe 0 -f concat -i super_cool_list.txt -c copy ..\Playlist.webm") 
+os.system(f'ffmpeg -safe 0 -f concat -i super_cool_list.txt -c copy "..\{playlist.get("title")}.webm"') 
 
 clear_working_path()
+
+print("Done, have a good day!")
